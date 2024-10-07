461aad9a6c67e323df912d43d0385845ab414264
mfolsom930 <86228082+mfolsom930@users.noreply.github.com>
2024-10-06 20:00:39 -0600

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,154 @@
-import Image from "next/image";
+"use client"
+
+import { useState } from 'react';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  
+
+  
+  
+  const [arr, setArr] = useState<Date[]>([]);
+  const [bool, setBool] = useState(false);
+  const [today, setToday] = useState<Date>(new Date());
+  const [first, setFirst] = useState<Date>(new Date());
+  const [prev, setPrev] = useState<Date>(new Date());
+  const [active, setActive] = useState<Date>(new Date());
+  const test = new Date();
+  
+
+  
+  const clickHandle = () => {
+    setArr([]);
+    const tempArr: Date[] = [];
+    
+    //Set first day of current month  
+    const makeFirstDay = prev.setDate(1);
+    const makeFirstDayDate = new Date(makeFirstDay);
+    const makeFirstMonth = makeFirstDayDate.setMonth(makeFirstDayDate.getMonth());
+    const makeFirst = new Date(makeFirstMonth);
+    setPrev(makeFirst);
+
+    let firstDay = prev.getDay();
+    const prevDate = prev;
+
+    //loop to add tail end days of previous month
+    while(firstDay){
+      const prevDateMS = prevDate.setDate(prevDate.getDate() - 1);
+      const prevDateDay = new Date(prevDateMS);
+      tempArr.unshift(prevDateDay)
+      firstDay--; 
+    }
+
+      //Set first day of current month 
+      const makeFirstDay2 = first.setDate(1);
+      const makeFirstDayDate2 = new Date(makeFirstDay2);
+      const makeFirstMonth2 = makeFirstDayDate2.setMonth(makeFirstDayDate2.getMonth());
+      const makeFirst2 = new Date(makeFirstMonth2);
+      setFirst(makeFirst2);
+      const currentDates = first;
+      let currentBool = false;
+
+      //loop to fill rest of calendar
+      while(tempArr.length < 35){
+        let currentDatesMS = 0;
+        if(!currentBool){
+           currentDatesMS = currentDates.setDate(currentDates.getDate());
+           currentBool = !currentBool;
+        }
+        else{
+
+          currentDatesMS = currentDates.setDate(currentDates.getDate() + 1);
+        }
+        
+        const currentDatesDay = new Date(currentDatesMS);
+        tempArr.push(currentDatesDay);
+      }
+     
+    
+    setArr(tempArr);
+    setBool(true);
+    
+  };
+
+  //view previous month
+  const clickHandlePast = () => {
+    const todayMinusMonthMS = today.setMonth(today.getMonth() - 1);
+    const todayMinusMonth = new Date(todayMinusMonthMS);
+    setToday(todayMinusMonth);
+
+    const testMinusMonthMS = prev.setMonth(prev.getMonth() - 1);
+    const testMinusMonth = new Date(testMinusMonthMS);
+    setPrev(testMinusMonth);
+
+    const firstMinusMonthMS = first.setMonth(first.getMonth() - 1);
+    const firstMinusMonth = new Date(firstMinusMonthMS);
+    setFirst(firstMinusMonth);
+
+    clickHandle();
+    
+    
+  }
+  
+  //view next month
+  const clickHandleFuture = () => {
+    const todayMinusMonthMS = today.setMonth(today.getMonth() + 1);
+    const todayMinusMonth = new Date(todayMinusMonthMS);
+    setToday(todayMinusMonth);
+
+    const testMinusMonthMS = prev.setMonth(prev.getMonth() + 1);
+    const testMinusMonth = new Date(testMinusMonthMS);
+    setPrev(testMinusMonth);
+
+    const firstMinusMonthMS = first.setMonth(first.getMonth() + 1);
+    const firstMinusMonth = new Date(firstMinusMonthMS);
+    setFirst(firstMinusMonth);
+
+    clickHandle();
+    
+  }
+  
+  //header stuff
+const headerDate = new Date(today);
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const month = months[headerDate.getMonth()];
+const year = headerDate.getFullYear();
+const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+
+const clickActive = (date: Date) => {
+  setActive(date);
+}
+
+  return (
+   <div className='cont'>
+      <a className='startBtn' onClick={clickHandle}>Start</a>
+      <div className='headerMonth'>
+        <h1>{year}</h1>
+        <div className='monthControls'>
+          <a className='btn' onClick={clickHandlePast}>&lt;</a>
+          <h1>{month}</h1>
+          <a className='btn' onClick={clickHandleFuture}>&gt;</a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <h1>{active.toLocaleDateString('en-US')}</h1>
+      </div>
+      <div className='calCont'>
+        {daysOfWeek.map((day, i) => (<h2 key={i}>{day}</h2>))}
+        {bool 
+        ? arr.map((item, index) => (
+        <div 
+        onClick={() => clickActive(item)}
+          className={item.getDate() === test.getDate() && item.getMonth() === test.getMonth() ? 'calItemToday' : 'calItem' } 
+          key={index}>
+            <div className={active === item ? 'calItemTodayActive' : 'calItem'}>
+            <p className={item.getMonth() === today.getMonth() ? 'activeMonth' : 'inactiveMonth'}>
+              {item.getDate()}
+            </p>
+            </div>
+          </div>)) 
+          : <></>}
+      </div>
+      
+   </div>
   );
 }
